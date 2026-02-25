d4b8e1cc340fa08dab0dd169f45f6a4a3d1a6899
Derek Roberts <derek.roberts@gmail.com>
2026-02-25 11:08:55 -0800

    fix: handle multiple potential PR numbers in commit message (#33)

--- get-pr/action.sh
+++ get-pr/action.sh
@@ -20,7 +20,13 @@ function get_pr_from_api_response() {
 
 function get_pr_from_git() {
   local commits=${1:-1}
-  git log --pretty=format:%s -${commits} 2>/dev/null | grep -o '#[0-9]\+' | grep -o '[0-9]\+' | head -1
+  git log --pretty=format:%s -${commits} 2>/dev/null | while read line; do
+    trimmed=$(echo "$line" | sed 's/[[:space:]]*$//')
+    if [[ $trimmed =~ \(#([0-9]+)\)$ ]]; then
+      echo "${BASH_REMATCH[1]}"
+      break
+    fi
+  done
 }
 
 # Process variables and inputs
